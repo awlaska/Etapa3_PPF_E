7659ba52baba6f29aea951f15c22ff290331272a
awlaska <79571817+awlaska@users.noreply.github.com>
2022-05-25 22:51:05 +0100

    macarena

--- src/app/p-inicial/p-inicial.page.ts
+++ src/app/p-inicial/p-inicial.page.ts
@@ -9,19 +9,39 @@ import { ProdutosService } from '../services/produtos.service';
 })
 export class PInicialPage implements OnInit {
 
-  constructor( public sapatoServ: ProdutosService ) { }
+  constructor( public sapatoServ: ProdutosService , public camisolaServ: ProdutosService ,  public acessorioServ: ProdutosService , public calcaServ: ProdutosService ) { }
 
   public sapatos: any;
+  public camisolas: any;
+  public acessorios: any;
+  public calcas: any;
   searchTerm: string;
   
   ngOnInit() {
     this.sapatoServ.getSapatos().subscribe(data => {
       this.sapatos = data;
     });
+    this.camisolaServ.getCamisolas().subscribe(data => {
+      this.camisolas = data;
+    });
+    this.acessorioServ.getAcessorios().subscribe(data => {
+      this.acessorios = data;
+    });
+    this.calcaServ.getCalcas().subscribe(data => {
+      this.calcas = data;
+    });
   }
 
   verSapato(marca: string, rota: string) {
     this.sapatoServ.goToRotaSapato(marca, rota);
   }
-
+  verCamisola(marca: string, rota: string) {
+    this.camisolaServ.goToRotaCamisola(marca, rota);
+  }
+  verAcessorio(marca: string, rota: string) {
+    this.acessorioServ.goToRotaAcessorio(marca, rota);
+  }
+  verCalca(marca: string, rota: string) {
+    this.calcaServ.goToRotaCalca(marca, rota);
+  }
 }
