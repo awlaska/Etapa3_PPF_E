6964774739424afc864e2d127f98f124ffa59a37
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2022-05-27 12:15:03 +0100

    form

--- src/app/selec-loja/selec-loja.module.ts
+++ src/app/selec-loja/selec-loja.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { SelecLojaPage } from './selec-loja.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SelecLojaPageRoutingModule
+    SelecLojaPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [SelecLojaPage]
 })
--- src/app/selec-loja/selec-loja.page.ts
+++ src/app/selec-loja/selec-loja.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { AlertController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-selec-loja',
@@ -10,11 +11,33 @@ import { AlertController } from '@ionic/angular';
 })
 export class SelecLojaPage implements OnInit {
 
-  constructor( private orientacao: ScreenOrientation, private route: Router, private alertCtrl: AlertController ) { 
+  LojaForm: FormGroup;
+  isSubmitted: boolean;
+
+  constructor( public formBuilder: FormBuilder, private orientacao: ScreenOrientation, private route: Router, private alertCtrl: AlertController ) { 
     this.orientacao.lock(this.orientacao.ORIENTATIONS.PORTRAIT);
+    this.isSubmitted = false;
   }
 
   ngOnInit() {
+    this.LojaForm = this.formBuilder.group({
+      loja: ['', [Validators.required]]
+   });
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.LojaForm.valid) {
+      return false;
+    } else {
+      this.finalizar();
+    }
+  }
+
+  // bind (ligação) de uma propriedade de um objeto a uma função
+  // quando a propriedade é "invocada" a função getter é utilizada
+  get formControls() { 
+    return this.LojaForm.controls;
   }
 
   home(){
